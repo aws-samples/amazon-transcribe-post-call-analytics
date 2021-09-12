d6517966ba76195da583f7644ac50bb6a60ed5d8
Bob Strahan <strahanr@amazon.com>
2021-09-12 22:05:13 +0000

    don't run lambda logic on CustomResource Delete action

--- pca-server/src/copy-samples/copy-samples.py
+++ pca-server/src/copy-samples/copy-samples.py
@@ -11,16 +11,17 @@ def lambda_handler(event, context):
     bucket = os.environ['INPUT_BUCKET_NAME']
     prefix = os.environ['INPUT_BUCKET_RAW_AUDIO']
     samples_dir = "./samples"
-    try:
-        s3Client = boto3.client('s3')
-        for subdir, dirs, files in os.walk(samples_dir):
-            for file in files:
-                file_path = os.path.join(subdir, file)
-                s3_key = os.path.join(prefix, file)
-                print(f"Uploading {file_path} to s3://{bucket}/{s3_key}")
-                response = s3Client.upload_file(file_path, bucket, s3_key)
-    except Exception as e:
-        print(e)
-        responseData["Error"] = f"Exception thrown: {e}"
-        status = cfnresponse.FAILED
+    if event['RequestType'] != 'Delete':
+        try:
+            s3Client = boto3.client('s3')
+            for subdir, dirs, files in os.walk(samples_dir):
+                for file in files:
+                    file_path = os.path.join(subdir, file)
+                    s3_key = os.path.join(prefix, file)
+                    print(f"Uploading {file_path} to s3://{bucket}/{s3_key}")
+                    response = s3Client.upload_file(file_path, bucket, s3_key)
+        except Exception as e:
+            print(e)
+            responseData["Error"] = f"Exception thrown: {e}"
+            status = cfnresponse.FAILED
     cfnresponse.send(event, context, status, responseData)
