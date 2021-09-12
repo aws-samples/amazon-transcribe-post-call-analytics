96e8e3241b86f5bee04b76f21055378e2dbe45bc
Bob Strahan <strahanr@amazon.com>
2021-09-12 18:13:22 +0000

    Add ability to optionally load sample audio files during insatallation

---
+++ pca-server/src/copy-samples/copy-samples.py
@@ -0,0 +1,26 @@
+import os
+import boto3
+import cfnresponse
+
+def lambda_handler(event, context):
+    """
+    Copy sample audio files to the input bucket for processing.
+    """
+    responseData={}
+    status = cfnresponse.SUCCESS
+    bucket = os.environ['INPUT_BUCKET_NAME']
+    prefix = os.environ['INPUT_BUCKET_RAW_AUDIO']
+    samples_dir = "./samples"
+    try:
+        s3Client = boto3.client('s3')
+        for subdir, dirs, files in os.walk(samples_dir):
+            for file in files:
+                file_path = os.path.join(subdir, file)
+                s3_key = os.path.join(prefix, file)
+                print(f"Uploading {file_path} to s3://{bucket}/{s3_key}")
+                response = s3Client.upload_file(file_path, bucket, s3_key)
+    except Exception as e:
+        print(e)
+        responseData["Error"] = f"Exception thrown: {e}"
+        status = cfnresponse.FAILED
+    cfnresponse.send(event, context, status, responseData)
