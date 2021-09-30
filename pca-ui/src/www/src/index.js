1565bd864741a8242508db3bff05fd02a92e549e
Connor Kirkpatrick <ckp@amazon.com>
2021-09-30 15:02:50 +0100

    Get auth working (kind of?)

--- pca-ui/src/www/src/index.js
+++ pca-ui/src/www/src/index.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { getToken, handleCode } from "./api/auth";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+(async () => {
+  try {
+    handleCode();
+
+    getToken();
+
+    renderApp();
+
+    //   if (!token) throw new Error("No token, auth required");
+    //   renderApp();
+  } catch (e) {
+    //   console.log(e);
+    //   if (typeof e === SessionExpired) refreshToken() && renderApp();
+    //   else redirectToLogin();
+  }
+})();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
