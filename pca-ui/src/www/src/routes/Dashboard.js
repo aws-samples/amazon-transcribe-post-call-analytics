31e02bcefeeb03e4690d5f8c92c4e6394f28ec4d
Connor Kirkpatrick <ckp@amazon.com>
2021-09-30 16:16:32 +0100

    initialise dashboard

--- pca-ui/src/www/src/routes/Dashboard.js
+++ pca-ui/src/www/src/routes/Dashboard.js
@@ -1,7 +1,25 @@
-function Dashboard () {
-  return (<div>
-    <h3>Dashboard</h3>
-  </div> )
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { get } from "../api/api";
+
+function Dashboard() {
+  const { key } = useParams();
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      const d = await get(key);
+      setData(d);
+    };
+    getData();
+  }, [key]);
+  return (
+    <div>
+      <h3>Dashboard</h3>
+      <h4>{key}</h4>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
