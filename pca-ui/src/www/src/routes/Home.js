c418cdb5f6d4ecccf4518bd7777cbaa2050ab4e3
Connor Kirkpatrick <ckp@amazon.com>
2021-09-30 15:45:17 +0100

    Add Summary Table

--- pca-ui/src/www/src/routes/Home.js
+++ pca-ui/src/www/src/routes/Home.js
@@ -1,7 +1,49 @@
-function Home () {
-  return (<div>
-    <h3>Home</h3>
-  </div> )
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import { list } from "../api/api";
+import config from "../config";
+
+function Home() {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await list({
+        count: config.api.pageSize,
+      });
+
+      setTableData(data);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h3>Home</h3>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Job Name</th>
+            <th>Timestamp</th>
+            <th>Average Accuracy</th>
+            <th>Language Code</th>
+            <th>Call Duration</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((row) => (
+            <tr>
+              <td>{row.jobName}</td>
+              <td>{row.timestamp}</td>
+              <td>{row.accuracy}</td>
+              <td>{row.lang}</td>
+              <td>{row.duration}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
