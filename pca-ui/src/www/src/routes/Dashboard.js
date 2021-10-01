3d7cebf991bae76b85f9968cafbba6368d3815da
Connor Kirkpatrick <ckp@amazon.com>
2021-10-01 16:04:28 +0100

    Create Dashboard outline

--- pca-ui/src/www/src/routes/Dashboard.js
+++ pca-ui/src/www/src/routes/Dashboard.js
@@ -1,6 +1,26 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { get } from "../api/api";
+import Card from "react-bootstrap/Card";
+import Stack from "react-bootstrap/Stack";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+// TODO
+// * Fix Value with label spacing
+// * Add rest of overview
+// * Add entities
+// * Add Transcipts
+
+const ValueWithLabel = ({ label, children }) => (
+  <div class="mb-l">
+    <h5 class="mb-s" color="text-label">
+      {label}
+    </h5>
+    <div>{children}</div>
+  </div>
+);
 
 function Dashboard() {
   const { key } = useParams();
@@ -18,6 +38,50 @@ function Dashboard() {
     <div>
       <h3>Dashboard</h3>
       <h4>{key}</h4>
+
+      <Stack direction="vertical" gap={4}>
+        <Card>
+          <Card.Body>
+            <Card.Title>Overview</Card.Title>
+            <Container>
+              <Row>
+                <Col>
+                  <ValueWithLabel label={"Timestamp"}>
+                    {data.ConversationAnalytics?.ConversationTime}
+                  </ValueWithLabel>
+                  <ValueWithLabel label={"Entity Recognizer Name"}>
+                    TODO
+                  </ValueWithLabel>
+                  <ValueWithLabel label={"Entity Recognizer Name"}>
+                    TODO
+                  </ValueWithLabel>
+                </Col>
+
+                <Col></Col>
+                <Col></Col>
+              </Row>
+            </Container>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Title>Entities</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Title>Transcript</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Stack>
     </div>
   );
 }
