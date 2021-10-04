e789636b952e752abe046a41f9443b067401be59
Connor Kirkpatrick <ckp@amazon.com>
2021-10-04 17:12:26 +0100

    Add format functions

--- pca-ui/src/www/src/format.js
+++ pca-ui/src/www/src/format.js
@@ -1,1 +1,23 @@
+export function Time(input) {
+  var mins = Math.floor(input / 60);
+  var secs = Math.floor(input - mins * 60).toLocaleString("en-GB", {
     maximumFractionDigits: 1,
+  });
+
+  return `${mins}`.padStart(2, "0") + ":" + `${secs}`.padStart(2, "0");
+}
+
+export function Percentage(input) {
+  return input.toLocaleString("en-GB", {
+    style: "percent",
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+  });
+}
+
+export function Number(input) {
+  return input.toLocaleString("en-GB", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+}
