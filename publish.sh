c912a884e6f80d0d9519413a2039f3c28ccca0d3
Connor Kirkpatrick <ckp@amazon.com>
2021-10-07 11:23:53 +0100

    Install and build UI

--- publish.sh
+++ publish.sh
@@ -64,6 +64,11 @@ pushd pca-ui/src/lambda
 npm install
 popd
 
+pushd pca-ui/src/www
+npm install
+npm build
+popd
+
 # Build and deploy embedded MediaSearch project
 pushd aws-kendra-transcribe-media-search
 if $PUBLIC; then
