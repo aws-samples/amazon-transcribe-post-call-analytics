6c9d269ad42a19f275254217c554cd62f863da76
Connor Kirkpatrick <ckp@amazon.com>
2021-10-28 17:13:19 +0100

    Remove duplication of highlighter

--- pca-ui/src/www/src/routes/Dashboard/TranscriptSegment.js
+++ pca-ui/src/www/src/routes/Dashboard/TranscriptSegment.js
@@ -9,38 +9,57 @@ export const TranscriptSegment = ({
   onClick,
   highlightLocations,
   score,
-}) => (
-  <Row>
-    <Col sm={1} className="pt-2">
-      <SentimentIcon score={score} />
-    </Col>
-    <Col>
-      <span className={"text-muted segment"}>
-        {name} -{" "}
-        <span
-          className="audio-start"
-          data-currenttime={segmentStart}
-          onClick={onClick}
-        >
-          {Formatter.Time(segmentStart)}
+}) => {
+  const t = highlightLocations.reduceRight(
+    (accumulator, { start, end, fn }, i) =>
+      replaceAt(accumulator, start, end, fn),
+    text
+  );
+  return (
+    <Row>
+      <Col sm={1} className="pt-2">
+        <SentimentIcon score={score} />
+      </Col>
+      <Col>
+        <span className={"text-muted segment"}>
+          {name} -{" "}
+          <span
+            className="audio-start"
+            data-currenttime={segmentStart}
+            onClick={onClick}
+          >
+            {Formatter.Time(segmentStart)}
+          </span>
         </span>
-      </span>
-      <div>
-        {highlightLocations.length
-          ? highlightLocations.map(({ start, end, fn }) =>
-              replaceAt(text, start, end, fn)
-            )
-          : text}
-      </div>
-    </Col>
-  </Row>
-);
+        <div>
+          {highlightLocations.length
+            ? highlightLocations.reduceRight(
+                (accumulator, { start, end, fn }, i) =>
+                  replaceAt(accumulator, start, end, fn),
+                text
+              )
+            : text}
+        </div>
+      </Col>
+    </Row>
+  );
+};
 
-export const replaceAt = (string, beginOffset, endOffset, fn) => {
+export const helper = (string, beginOffset, endOffset, fn) => {
   let before = string.slice(0, beginOffset);
   let after = string.slice(endOffset);
 
   const target = string.slice(beginOffset, endOffset);
   const replaced = fn(target);
-  return [before, replaced, after];
+  return [before, replaced, after].flat();
+};
+
+const replaceAt = (input, ...opts) => {
+  if (!Array.isArray(input)) input = [input];
+
+  return input
+    .map((chunk) =>
+      typeof chunk === "string" ? helper(chunk, ...opts) : chunk
+    )
+    .flat();
 };
