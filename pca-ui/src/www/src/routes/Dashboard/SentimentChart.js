d89c3104b7144e09c9a1b706c8f0603ca6454cc8
Connor Kirkpatrick <ckp@amazon.com>
2021-10-28 09:31:27 +0100

    Add sentiment over time chart

---
+++ pca-ui/src/www/src/routes/Dashboard/SentimentChart.js
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+
+export const SentimentChart = ({ data = [], speakerOrder = {} }) => {
+  const colours = {
+    Agent: "hsl(45, 100%, 50%)",
+    Caller: "hsl(202, 100%, 50%)",
+  };
+
+  const firstUtterance = {
+    ...Object.keys(speakerOrder).reduce(
+      (prev, curr) => ({ ...prev, [curr]: getFirstUtterance(curr, data) }),
+      {}
+    ),
+  };
+
+  const lastUtterance = {
+    ...Object.keys(speakerOrder).reduce(
+      (prev, curr) => ({ ...prev, [curr]: getLastUtterance(curr, data) }),
+      {}
+    ),
+  };
+
+  return (
+    <Line
+      data={{
+        labels: data.map((part) => {
+          return Math.floor(part.SegmentStartTime);
+        }),
+
+        datasets: Object.keys(speakerOrder).map((speaker) => {
+          return {
+            label: speakerOrder[speaker],
+            borderColor: colours[speakerOrder[speaker]],
+            fill: false,
+            spanGaps: true,
+            data: data.map((part, i) => {
+              return getPoint(speaker, firstUtterance, lastUtterance, part);
+            }),
+          };
+        }),
+      }}
+      option={options}
+    />
+  );
+};
+
+const options = {
+  scales: {
+    xAxes: [
+      {
+        display: false,
+      },
+    ],
+    yAxes: [
+      {
+        display: true,
+      },
+    ],
+  },
+  legend: {
+    display: true,
+  },
+  title: {
+    text: "Call Sentiment over time",
+    display: true,
+    position: "bottom",
+  },
+};
+
+const getPoint = (speaker, firstUtterance, lastUtterance, part) => {
+  if (part.SegmentSpeaker !== speaker) {
+    return null;
+  }
+
+  if (part.SentimentIsPositive) {
+    return part.SentimentScore;
+  }
+
+  if (part.SentimentIsNegative) {
+    return -part.SentimentScore;
+  }
+
+  if (
+    part.SegmentStartTime === firstUtterance[speaker] ||
+    part.SegmentStartTime === lastUtterance[speaker]
+  ) {
+    return 0;
+  }
+
+  return null;
+};
+
+const getFirstUtterance = (speaker, parts) => {
+  return parts
+    .filter((part) => part.SegmentSpeaker === speaker)
+    .reduce(
+      (prev, curr) =>
+        prev <= curr.SegmentStartTime ? prev : curr.SegmentStartTime,
+      parts?.[0]?.SegmentStartTime
+    );
+};
+const getLastUtterance = (speaker, parts) => {
+  return parts
+    .filter((part) => part.SegmentSpeaker === speaker)
+    .reduce(
+      (prev, curr) =>
+        prev >= curr.SegmentStartTime ? prev : curr.SegmentStartTime,
+      parts?.[0]?.SegmentStartTime
+    );
+};
