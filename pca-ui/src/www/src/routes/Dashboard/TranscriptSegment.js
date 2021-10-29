f9b8209839056ef90c98c73cca9dd6c5d9a799f4
Connor Kirkpatrick <ckp@amazon.com>
2021-10-29 14:58:31 +0100

    Add basic highlighting to transcript segment
    This commit adds logic for highlighting elements in the transcript.
    
    In future this might have a variety of uses, but for now it is used to highlight entities.
    
    This approach has a limitation that replacement instructions should be ordered from lowest start offset to highest.
    Additionally, substrings identified by start and end offsets cannot overlap.
    
    The replacement is applied from right to left. This avoids the problem of changing the string offset, which would occur if the replacements were applied from left to right.
    
    I've not codified these assertions into the function itself
    
    I wanted to keep this PR small, so all entities are highlighted red. In a future PR they will be highlighted different colours according to type.

--- pca-ui/src/www/src/routes/Dashboard/TranscriptSegment.js
+++ pca-ui/src/www/src/routes/Dashboard/TranscriptSegment.js
@@ -8,7 +8,6 @@ export const TranscriptSegment = ({
   text,
   onClick,
   highlightLocations,
-  highlightFunc,
   score,
 }) => (
   <Row>
@@ -26,7 +25,42 @@ export const TranscriptSegment = ({
           {Formatter.Time(segmentStart)}
         </span>
       </span>
-      <p>{text}</p>
+      <div>{applyReplacements(text, highlightLocations)}</div>
     </Col>
   </Row>
 );
+
+// substituteAt replaces a subset of a string with the value of the fn provided.
+// it returns an array containing string and react elements
+const substituteAt = (input, beginOffset, endOffset, fn, key) => {
+  let before = input.slice(0, beginOffset);
+  let after = input.slice(endOffset);
+
+  const target = input.slice(beginOffset, endOffset);
+  const replaced = fn(target, key);
+  return [before, replaced, after];
+};
+
+const wrapper = (input, ...opts) => {
+  if (!Array.isArray(input)) input = [input];
+
+  return input
+    .map((chunk, i) =>
+      typeof chunk === "string" && i === 0
+        ? substituteAt(chunk, ...opts)
+        : chunk
+    )
+    .flat();
+};
+
+// applyReplacements applies a series of string replacements to the input.
+// each replacement should consist of a start offset, end offset and
+// function that transforms the target string
+// replacements should be ordered from lowest start offset to highest.
+// Substrings identified by start and end offsets cannot overlap
+export const applyReplacements = (input, replacements) =>
+  replacements.reduceRight(
+    (accumulator, { start, end, fn }, i) =>
+      wrapper(accumulator, start, end, fn, i),
+    input
+  );
