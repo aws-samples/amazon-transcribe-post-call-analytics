4b478d7584fe2b30300e9442c71ae257dee18005
Connor Kirkpatrick <ckp@amazon.com>
2021-11-03 15:14:47 +0000

    Add entity fallback

--- pca-ui/src/www/src/routes/Dashboard/Entities.js
+++ pca-ui/src/www/src/routes/Dashboard/Entities.js
@@ -1,32 +1,35 @@
 import { Badge, ListGroup, Tab, Tabs } from "react-bootstrap";
 
-export const Entities = ({ data }) => (
-  <Tabs
-    defaultActiveKey={data[0].Name}
-    id="entitities-tab-group"
-    className="mb-3"
-  >
-    {data.map((e, i) => (
-      <Tab
-        key={i}
-        eventKey={e.Name}
-        title={
-          <span>
-            {e.Name}{" "}
-            <Badge bg="secondary" pill={true}>
-              {e.Instances}
-            </Badge>
-          </span>
-        }
-      >
-        <ListGroup variant="flush">
-          {e.Values.map((v, i) => (
-            <ListGroup.Item key={i}>
-              <p>{v}</p>
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-      </Tab>
-    ))}
-  </Tabs>
-);
+export const Entities = ({ data }) =>
+  data.length ? (
+    <Tabs
+      defaultActiveKey={data[0].Name}
+      id="entitities-tab-group"
+      className="mb-3"
+    >
+      {data.map((e, i) => (
+        <Tab
+          key={i}
+          eventKey={e.Name}
+          title={
+            <span>
+              {e.Name}{" "}
+              <Badge bg="secondary" pill={true}>
+                {e.Instances}
+              </Badge>
+            </span>
+          }
+        >
+          <ListGroup variant="flush">
+            {e.Values.map((v, i) => (
+              <ListGroup.Item key={i}>
+                <p>{v}</p>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Tab>
+      ))}
+    </Tabs>
+  ) : (
+    <p>No entities detected</p>
+  );
