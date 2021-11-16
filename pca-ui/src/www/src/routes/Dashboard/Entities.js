db67ae5bfab80a001bb19739b42439b5bd83bb48
Connor Kirkpatrick <ckp@amazon.com>
2021-11-16 10:04:51 +0000

    Add tag component (#48)
    * Add tag component
    * Add defense to function
    * Add key to entities

--- pca-ui/src/www/src/routes/Dashboard/Entities.js
+++ pca-ui/src/www/src/routes/Dashboard/Entities.js
@@ -1,35 +1,35 @@
-import { Tab, Tabs } from "react-bootstrap";
-import { ListItems } from "./ListItems";
+import { ValueWithLabel } from "../../components/ValueWithLabel";
+import { Tag } from "../../components/Tag";
 import "./Entities.css";
 
-const Box = ({ children, className }) => (
-  <div className={`box ${className}`}>{children}</div>
-);
-
-export const Entities = ({ data }) =>
-  data.length ? (
-    <Tabs
-      defaultActiveKey={data[0].Name}
-      id="entitities-tab-group"
-      className="mb-3"
-    >
-      {data.map((e, i) => (
-        <Tab
-          key={i}
-          eventKey={e.Name}
-          title={
-            <span>
-              {e.Name}{" "}
-              <Box className={`ms-1 highlight ${e.Name.toLowerCase()}`}>
-                {e.Instances}
-              </Box>
-            </span>
-          }
-        >
-          <ListItems data={e.Values} />
-        </Tab>
-      ))}
-    </Tabs>
+export const Entities = ({ data }) => {
+  return data.length ? (
+    data.map((e, i) => (
+      <ValueWithLabel key={i} label={<span>{toSentenceCase(e.Name)}</span>}>
+        {e.Values.map((x, j) => (
+          <Tag
+            key={j}
+            className="me-2 mb-1"
+            color={`var(--entity-${e.Name.toLowerCase()}`}
+          >
+            {x}
+          </Tag>
+        ))}
+      </ValueWithLabel>
+    ))
   ) : (
     <p>No entities detected</p>
   );
+};
+
+const toSentenceCase = (word) => {
+  if (typeof word !== "string") {
+    return "";
+  }
+
+  if (word.length <= 1) {
+    return word.toUpperCase();
+  }
+
+  return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+};
