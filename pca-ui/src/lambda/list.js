848a62d5b2cfcc49c46e933ca6aff943f28892e2
Connor Kirkpatrick <ckp@amazon.com>
2021-11-19 11:39:55 +0000

    Paginate List API and Homepage (#49)
    * Update API
    * Handle new list format in UI
    * Provide startkey and start timestamp
    * Add pagination

--- pca-ui/src/lambda/list.js
+++ pca-ui/src/lambda/list.js
@@ -6,81 +6,91 @@ const tableName = process.env.TableName;
 const DEFAULT_COUNT = 100;
 
 exports.handler = async function (event, context) {
-    console.log(
-        JSON.stringify(
-            {
-                event: event,
-                context: context,
-            },
-            null,
-            4
-        )
-    );
+  console.log(
+    JSON.stringify(
+      {
+        event: event,
+        context: context,
+      },
+      null,
+      4
+    )
+  );
 
-    let start = null;
-    let count = DEFAULT_COUNT;
+  let start = null;
+  let count = DEFAULT_COUNT;
 
-    if (event.queryStringParameters != null) {
-        if (
-            "startKey" in event.queryStringParameters &&
-            "startTimestamp" in event.queryStringParameters
-        ) {
-            start = {
-                PK: {
-                    S: `call#${event.queryStringParameters.startKey}`,
-                },
-                SK: {
-                    S: "call",
-                },
-                TK: {
-                    N: event.queryStringParameters.startTimestamp,
-                },
-            };
-        }
-
-        if ("count" in event.queryStringParameters) {
-            count = event.queryStringParameters.count;
-        }
-    }
-
-    let query = {
-        TableName: tableName,
-        IndexName: "GSI1",
-        ScanIndexForward: "reverse" in event.queryStringParameters,
-        Limit: count,
-        KeyConditionExpression: "SK = :sk",
-        ExpressionAttributeValues: {
-            ":sk": {
-                S: "call",
-            },
+  if (event.queryStringParameters != null) {
+    if (
+      "startKey" in event.queryStringParameters &&
+      "startTimestamp" in event.queryStringParameters
+    ) {
+      start = {
+        PK: {
+          S: `call#${event.queryStringParameters.startKey}`,
         },
-    };
-
-    if (start != null) {
-        query.ExclusiveStartKey = start;
-    }
-
-    console.log("Query:", JSON.stringify(query, null, 4));
-
-    let res;
-    try {
-        res = await ddb.query(query).promise();
-    } catch (e) {
-        throw e;
-    }
-    console.log(res);
-
-    return {
-        statusCode: 200,
-        headers: {
-            "access-control-allow-origin": "*",
-            "access-control-allow-headers": "Content-Type,Authorization",
-            "access-control-allow-methods": "OPTIONS,GET",
+        SK: {
+          S: "call",
         },
-        body: JSON.stringify(
-            res.Items.map((item) => {
-                return JSON.parse(item.Data.S);
-            })
-        ),
-    };
+        TK: {
+          N: event.queryStringParameters.startTimestamp,
+        },
+      };
+    }
+
+    if ("count" in event.queryStringParameters) {
+      count = event.queryStringParameters.count;
+    }
+  }
+
+  let query = {
+    TableName: tableName,
+    IndexName: "GSI1",
+    ScanIndexForward: "reverse" in event.queryStringParameters,
+    Limit: count,
+    KeyConditionExpression: "SK = :sk",
+    ExpressionAttributeValues: {
+      ":sk": {
+        S: "call",
+      },
+    },
+  };
+
+  if (start != null) {
+    query.ExclusiveStartKey = start;
+  }
+
+  console.log("Query:", JSON.stringify(query, null, 4));
+
+  let res;
+  try {
+    res = await ddb.query(query).promise();
+  } catch (e) {
+    throw e;
+  }
+  console.log(res);
+
+  const resp = {
+    statusCode: 200,
+    headers: {
+      "access-control-allow-origin": "*",
+      "access-control-allow-headers": "Content-Type,Authorization",
+      "access-control-allow-methods": "OPTIONS,GET",
+    },
+  };
+
+  const body = {
+    Records: res.Items.map((item) => {
+      return JSON.parse(item.Data.S);
+    }),
+  };
+
+  if (res.LastEvaluatedKey) {
+    body.StartKey = res.LastEvaluatedKey.PK.S;
+    body.StartTimestamp = res.LastEvaluatedKey.TK.N;
+  }
+
+  console.log({ resp });
+
+  return { ...resp, body: JSON.stringify(body) };
 };
--- pca-ui/src/www/src/routes/Home.js
+++ pca-ui/src/www/src/routes/Home.js
@@ -1,21 +1,69 @@
 import React from "react";
-import useSWR from "swr";
+import useSWRInfinite from "swr/infinite";
 import { list } from "../api/api";
 import { ContactTable } from "../components/ContactTable";
 import { useDangerAlert } from "../hooks/useAlert";
+import { Button } from "react-bootstrap";
 
 const config = window.pcaSettings;
 
 function Home({ setAlert }) {
-  const fetcher = () => list({ count: config.api.pageSize });
-  const { data, error } = useSWR(`/list`, fetcher);
+  const fetcher = (url, startKey, startTimestamp) => {
+    const opts = {
+      count: config.api.pageSize,
+    };
 
+    if (startTimestamp) opts.startTimestamp = startTimestamp;
+    if (startKey) opts.startKey = startKey;
+    return list(opts);
+  };
+
+  const getKey = (pageIndex, previousPageData) => {
+    console.log({ pageIndex, previousPageData });
+    if (previousPageData && !previousPageData.StartKey) return null;
+    if (pageIndex === 0) return `/list`;
+
+    const { StartKey, StartTimestamp } = previousPageData;
+
+    return [
+      `/list?startKey=${StartKey}&startTimestamp=${StartTimestamp}`,
+      StartKey,
+      StartTimestamp,
+    ];
+  };
+
+  const { data, error, size, setSize } = useSWRInfinite(getKey, fetcher);
+
+  const isLoadingInitialData = !data && !error;
+  const isLoadingMore =
+    isLoadingInitialData ||
+    (size > 0 && data && typeof data[size - 1] === "undefined");
+  const isEmpty = data?.[0]?.length === 0;
+  const isReachingEnd =
+    isEmpty || (data && data[data.length - 1]?.length < config.api.pageSize);
+
+  const details = (data || []).map((d) => d.Records).flat();
   useDangerAlert(error, setAlert);
 
   return (
     <div>
       <h3>Home</h3>
-      <ContactTable data={data} loading={!data && !error} empty={<Empty />} />
+      <ContactTable
+        data={details}
+        loading={!data && !error}
+        empty={<Empty />}
+      />
+      <Button
+        variant="primary"
+        onClick={() => setSize(size + 1)}
+        disabled={isLoadingMore || isReachingEnd}
+      >
+        {isLoadingMore
+          ? "Loading..."
+          : isReachingEnd
+          ? "No more to load"
+          : "Load more"}
+      </Button>
     </div>
   );
 }
