1a79081deef41798fb981f22e0246ee1b6e99e12
Connor Kirkpatrick <ckp@amazon.com>
2021-12-02 16:37:57 +0000

    Add Interruptions

--- pca-ui/src/www/src/routes/Dashboard/LoudnessChart.js
+++ pca-ui/src/www/src/routes/Dashboard/LoudnessChart.js
@@ -2,48 +2,53 @@ import { colours } from "./colours";
 import { Bar } from "react-chartjs-2";
 
 export const LoudnessChart = ({ data, caller }) => {
-  return (
-    console.log({ data }) || (
-      <Bar
-        data={{
-          labels: data.map((i) => i.x),
-          datasets: [
-            {
-              label: "Loudness",
-              data: data,
-              backgroundColor: colours[caller],
-              borderColor: colours[caller],
-            },
-            {
-              data: data
-                .filter((d) => d.interruption)
-                .map((d) => ({ y: d.interruption, x: d.x })),
-              label: "interruption",
-              backgroundColor: "yellow",
-              skipNull: true,
-              type: "bar",
-            },
-          ],
-        }}
-        options={{
-          scales: {
-            x: {
-              stacked: true,
-              display: true,
-              position: "left",
-              title: { text: "Minute", display: true },
-            },
 
-            y: {
-              display: true,
-              stacked: false,
-              position: "left",
-              title: { text: "Decibels", display: true },
-            },
+  const interruptions = data
+    .filter((d) => d.interruption)
+    .map((d) => ({ y: d.interruption, x: d.x }));
+
+  return (
+    <Bar
+      data={{
+        labels: data.map((i) => i.x),
+        datasets: [
+          {
+            label: "Loudness",
+            data: data,
+            backgroundColor: colours[caller],
+            borderColor: colours[caller],
+            barThickness: 20,
+            order: 1,
           },
-          plugins: { legend: { display: false } },
-        }}
-      />
-    )
+          {
+            data: interruptions,
+            label: "Interruption",
+            backgroundColor: "rgba(252,232,83,0.6)",
+            skipNull: true,
+            type: "bar",
+            barThickness: 30,
+            order: 1,
+          },
+        ],
+      }}
+      options={{
+        scales: {
+          x: {
+            stacked: true,
+            display: true,
+            position: "left",
+            title: { text: "Minute", display: true },
+          },
+
+          y: {
+            display: true,
+            stacked: false,
+            position: "left",
+            title: { text: "Decibels", display: true },
+          },
+        },
+        plugins: { legend: { display: false } },
+      }}
+    />
   );
 };
