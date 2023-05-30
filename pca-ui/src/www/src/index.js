b1610087e318de63c400ce7cc35de554bd6420ed
Christopher Lott <cmlott@amazon.com>
2023-05-29 20:29:56 -0700

    start work on cloudscape

--- pca-ui/src/www/src/index.js
+++ pca-ui/src/www/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "@cloudscape-design/global-styles/index.css"
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 import {
   parseAuthQueryString,
