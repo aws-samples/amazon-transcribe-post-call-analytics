9af90994333e90cdc1e0564432d5724e82881750
Christopher Lott <cmlott@amazon.com>
2023-05-29 20:29:56 -0700

    start work on cloudscape

--- pca-ui/src/www/src/components/ContactTable.js
+++ pca-ui/src/www/src/components/ContactTable.js
@@ -1,4 +1,7 @@
-import { Table } from "react-bootstrap";
+//import { Table } from "react-bootstrap";
+import React from "react";
+import { useCollection } from '@cloudscape-design/collection-hooks';
+import { Table, TextFilter, Pagination, CollectionPreferences } from '@cloudscape-design/components';
 import { useHistory } from "react-router-dom";
 import { Formatter } from "../format";
 import { Placeholder } from "./Placeholder";
@@ -8,31 +11,40 @@ import "./ContactTable.css";
 
 const columns = [
   {
-    label: "Job Name",
-    value: (d) => d.jobName,
+    header: "Job Name",
+    cell: (d) => d.jobName,
+    isRowHeader: true,
+    sortingField: "jobName",
+    minWidth:300
   },
-  { label: "Timestamp", value: (d) => Formatter.Timestamp(d.timestamp) },
   {
-    label: (
-      <div className="col-header-wrapper text-center">Customer Sentiment</div>
-    ),
-    value: (d) => (
+    header: "Timestamp",
+    cell: (d) => Formatter.Timestamp(d.timestamp)
+  },
+  {
+    header: "Cust Sent",
+    cell: (d) => (
       <div className="d-flex justify-content-evenly">
         <SentimentIcon score={d?.callerSentimentScore} />
         <TrendIcon trend={d.callerSentimentChange} />
       </div>
     ),
+    width: 150
   },
 
   {
-    label: <div className="col-header-wrapper text-left">Language Code</div>,
-    value: (d) => d.lang,
+    header: <div className="col-header-wrapper text-left">Language Code</div>,
+    cell: (d) => d.lang,
   },
   {
-    label: <div className="col-header-wrapper text-left">Call Duration</div>,
-    value: (d) => Formatter.Time(d.duration),
+    header: <div className="col-header-wrapper text-left">Call Duration</div>,
+    cell: (d) => Formatter.Time(d.duration),
   },
 ];
+
+const getMatchesCountText = function getMatchesCountText(count) {
+  return count === 1 ? `1 match` : `${count} matches`;
+}
 
 const Loading = () =>
   columns.map((c, i) => (
@@ -53,39 +65,52 @@ export const ContactTable = ({ data = [], loading = false, empty }) => {
   const history = useHistory();
 
   const onClick = (e) => {
-    history.push(`/dashboard/${e.key}`);
+    console.log(e);
+    history.push(`/dashboard/${e.detail.item.key}`);
   };
+  const [
+    filteringText,
+    setFilteringText
+  ] = React.useState("");
+
+  const { items, actions, filteredItemsCount, collectionProps, filterProps, paginationProps } = useCollection(
+    data,
+    {
+      filtering: {
+        empty: (
+          <div>No Calls.</div>
+        ),
+        noMatch: (
+          <div>No matches.</div>
+        )
+      },
+      pagination: { pageSize: 30 },
+      sorting: {},
+      selection: {},
+    });
 
   return (
-    <Table hover striped>
-      <thead>
-        <tr>
-          {columns.map((c, i) => (
-            <th className="text-muted text-uppercase" key={i}>
-              {c.label}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {loading ? (
-          <tr>
-            <Loading />
-          </tr>
-        ) : data.length === 0 ? (
-          <NoMatches children={empty} />
-        ) : (
-          data.map((row, i) => (
-            <tr className="contact-table" key={i} onClick={(e) => onClick(row)}>
-              {columns.map((c, j) => (
-                <td className="fs-5 col" key={j}>
-                  {c.value(row) || "-"}
-                </td>
-              ))}
-            </tr>
-          ))
-        )}
-      </tbody>
-    </Table>
+    <Table
+      {...collectionProps}
+      columnDefinitions={columns}
+      items={items}
+      pagination={<Pagination {...paginationProps} />}
+      resizableColumns={true}
+      loadingText="Loading Calls"
+      onRowClick={onClick}
+      filter={
+        <TextFilter
+          {...filterProps}
+          countText={getMatchesCountText(filteredItemsCount)}
+          filteringAriaLabel="Filter calls"
+          filteringPlaceholder="Find calls"
+          filteringClearAriaLabel="Clear"
+          /* filteringText={filteringText}
+          onChange={({ detail }) =>
+            setFilteringText(detail.filteringText)
+          }*/
+        />
+      }
+    />
   );
 };
