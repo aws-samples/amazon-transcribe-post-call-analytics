eb3fbd00c5de61030b02783f2a16422ea03613e2
Christopher Lott <cmlott@amazon.com>
2023-06-07 20:11:01 -0700

    fix errors, including audio playback

--- pca-ui/src/www/src/components/ValueWithLabel.js
+++ pca-ui/src/www/src/components/ValueWithLabel.js
@@ -1,10 +1,10 @@
 import { Box, Container, Header, Link, SpaceBetween, StatusIndicator } from '@cloudscape-design/components';
 
 export const ValueWithLabel = ({ label, children }) => (
-  <div>
+  <>
     <Box variant="awsui-key-label">
       {label}
     </Box>
-    <div>{children}</div>
-  </div>
+    <>{children}</>
+  </>
 );
