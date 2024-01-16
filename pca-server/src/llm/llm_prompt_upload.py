e867005ec2ba918b671ff6f6e1a4f6c0bc30bc62
Christopher Lott <cmlott@amazon.com>
2024-01-16 21:04:07 +0000

    fix llm prompt upload to fail gracefully and fix issue with json-string conversion

--- pca-server/src/llm/llm_prompt_upload.py
+++ pca-server/src/llm/llm_prompt_upload.py
@@ -3,6 +3,7 @@ import cfnresponse
 import json
 
 def lambda_handler(event, context):
+    print(event)
     # Init ...
     the_event = event['RequestType']
     print("The event is: ", str(the_event))
@@ -16,60 +17,60 @@ def lambda_handler(event, context):
     table = dynamodb.Table(table_name)
     ssm_client = boto3.client('ssm')
 
-    # First look if LLMPromptSummaryTemplate parameter exists in the Parameter Store, so we can
-    # migrate the template to DynamoDB. This is to preserve backwards compatibility when users
-    # update their stack.
-
-    try:
-        summary_prompt_template = ssm_client.get_parameter(Name="LLMPromptSummaryTemplate")["Parameter"]["Value"]
-    except Exception as e:
-        print("No parameter found:", str(e))
-        summary_prompt_template = json.dumps(llm_prompt_summary_template)
-
-    try:
-      summary_prompt_template = json.loads(summary_prompt_template)
-    except Exception as e:
-      print("Not a valid JSON:", str(e))
-      summary_prompt_template = {"Summary": summary_prompt_template}
-
-    update_expression = "SET"
-    expression_attribute_names = {}
-    expression_attribute_values = {}
-
-    i = 1
-    for key, value in summary_prompt_template.items():
-        update_expression += f" #{i} = :{i},"
-        expression_attribute_names[f"#{i}"] = f"{i}#{key}"
-        expression_attribute_values[f":{i}"] = value
-        i += 1
-
-    update_expression = update_expression[:-1] # remove last comma
-
-    # Next look if LLMPromptQueryTemplate parameter exists in the Parameter Store, so we can
-    # migrate the template to DynamoDB. This is to preserve backwards compatibility when users
-    # update their stack.
-
-    try:
-        query_prompt_template = ssm_client.get_parameter(Name="LLMPromptQueryTemplate")["Parameter"]["Value"]
-    except Exception as e:
-        print("No parameter found:", str(e))
-        query_prompt_template = llm_prompt_query_template
-
     try:
         if the_event in ('Create'):
-          response = table.update_item(
-                      Key={'LLMPromptTemplateId': 'LLMPromptSummaryTemplate'},
-                      UpdateExpression=update_expression,
-                      ExpressionAttributeValues=expression_attribute_values,
-                      ExpressionAttributeNames=expression_attribute_names
-                    )
+            # First look if LLMPromptSummaryTemplate parameter exists in the Parameter Store, so we can
+            # migrate the template to DynamoDB. This is to preserve backwards compatibility when users
+            # update their stack.
+        
+            try:
+                summary_prompt_template_str = ssm_client.get_parameter(Name="LLMPromptSummaryTemplate")["Parameter"]["Value"]
+            except Exception as e:
+                print("No parameter found:", str(e))
+                summary_prompt_template_str = llm_prompt_summary_template
+        
+            try:
+                summary_prompt_template = json.loads(summary_prompt_template_str)
+            except Exception as e:
+                print("Not a valid JSON:", str(e))
+                summary_prompt_template = {"Summary": summary_prompt_template_str}
+        
+            update_expression = "SET"
+            expression_attribute_names = {}
+            expression_attribute_values = {}
+        
+            i = 1
+            for key, value in summary_prompt_template.items():
+                update_expression += f" #{i} = :{i},"
+                expression_attribute_names[f"#{i}"] = f"{i}#{key}"
+                expression_attribute_values[f":{i}"] = value
+                i += 1
+        
+            update_expression = update_expression[:-1] # remove last comma
+        
+            # Next look if LLMPromptQueryTemplate parameter exists in the Parameter Store, so we can
+            # migrate the template to DynamoDB. This is to preserve backwards compatibility when users
+            # update their stack.
+        
+            try:
+                query_prompt_template = ssm_client.get_parameter(Name="LLMPromptQueryTemplate")["Parameter"]["Value"]
+            except Exception as e:
+                print("No parameter found:", str(e))
+                query_prompt_template = llm_prompt_query_template    
 
-          item = {
-            'LLMPromptTemplateId': 'LLMPromptQueryTemplate',
-            'LLMPromptTemplateValue': query_prompt_template
-          }
+            response = table.update_item(
+                  Key={'LLMPromptTemplateId': 'LLMPromptSummaryTemplate'},
+                  UpdateExpression=update_expression,
+                  ExpressionAttributeValues=expression_attribute_values,
+                  ExpressionAttributeNames=expression_attribute_names
+                )
 
-          response = table.put_item(Item=item)
+            item = {
+                'LLMPromptTemplateId': 'LLMPromptQueryTemplate',
+                'LLMPromptTemplateValue': query_prompt_template
+            }
+
+            response = table.put_item(Item=item)
 
         # Everything OK... send the signal back
         print("Operation successful!")
