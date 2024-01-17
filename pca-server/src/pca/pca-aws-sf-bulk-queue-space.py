cf0878ad0fc1e474127f4ef7a2b910c3f3ab902e
Christopher Lott <cmlott@amazon.com>
2024-01-17 01:36:09 +0000

    Fixes issue #224 where bulk flow is not counting call analytics jobs, but only standard transcribe jobs.

--- pca-server/src/pca/pca-aws-sf-bulk-queue-space.py
+++ pca-server/src/pca/pca-aws-sf-bulk-queue-space.py
@@ -23,6 +23,13 @@ def countTranscribeJobsInState(status, client, filesLimit):
         response = client.list_transcription_jobs(Status=status, NextToken=response["NextToken"])
         found += len(response["TranscriptionJobSummaries"])
 
+    # now count the call analytics jobs
+    response = client.list_call_analytics_jobs(Status=status)
+    found += len(response["CallAnalyticsJobSummaries"])
+    while ("NextToken" in response) and (found <= filesLimit):
+        response = client.list_call_analytics_jobs(Status=status, NextToken=response["NextToken"])
+        found += len(response["CallAnalyticsJobSummaries"])
+
     return found
 
 
