ad680b669bb7e065ac220b86e4d274a63fc738bc
rstrahan <strahanr@amazon.com>
2024-03-06 15:33:32 +0000

    Added submodule submodule-mediasearch

--- aws-kendra-transcribe-media-search/src/search/facets/AvailableFacetRetriever.ts
+++
@@ -1,6 +0,0 @@
-import Kendra from "aws-sdk/clients/kendra";
-
-export interface AvailableFacetRetriever {
-    get(attributeName: string): Kendra.DocumentAttributeValueCountPair[];
-    getAvailableAttributeNames(): string[];
-}
