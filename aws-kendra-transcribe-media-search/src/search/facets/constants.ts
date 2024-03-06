7e6805fe327ed6b6906bd387dbeb546ef01cb3ce
rstrahan <strahanr@amazon.com>
2024-03-06 15:33:32 +0000

    Added submodule submodule-mediasearch

--- aws-kendra-transcribe-media-search/src/search/facets/constants.ts
+++
@@ -1,7 +0,0 @@
-export const i18n = {
-  SHOW_MORE: "Show more...",
-  SHOW_LESS: "Show less...",
-  CLEAR: "Clear",
-  FILTER_SEARCH_RESULTS: "Filter search results",
-  RESET_FILTERS: "Reset filters",
-};
